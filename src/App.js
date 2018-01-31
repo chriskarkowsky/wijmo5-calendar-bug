5ae3d47d7c7865f06b50408b866e083030550522
Chris Karkowsky <chriskarkowsky@Chriss-MacBook-Pro.local>
2018-01-31 14:08:38 -0500

    another sample

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import './wijmo.css';
 import moment from 'moment';
-import { Calendar } from 'wijmo/wijmo.react.input';
+import { Calendar, InputNumber } from 'wijmo/wijmo.react.input';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      MinEndDate: moment()
-      
-        .subtract('days', 3)
-        .startOf('day')
-        .format(),
-      MaxEndDate: moment()
-        .subtract('days', 2)
-        .startOf('day')
-        .format(),
-      Today: moment().format(),
-   }
+      StartDate: moment("1/15/2018"),
+      EndDate: moment("1/15/2018").add("days",7),
+    };
+    this.startDateChanged = this.startDateChanged.bind(this);
+  }
+
+  startDateChanged(args) {
+    this.setState({
+      StartDate: moment(args.value),
+      EndDate: moment(args.value).add("days",7),
+    });
   }
 
   render() {
@@ -32,22 +32,30 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
+        <p>Pick a Start Date</p>
+        <p>Start Date: {this.state.StartDate.format()}</p>
         <Calendar
           className="wijCalendar1"
-          /*Incorrect*/
-          min={this.state.MinEndDate}
-          max={this.state.MaxEndDate}
-          value={this.state.Today}
+          min={moment("1/1/2018").format()}
+          max={moment("2/1/2018").format()}
+          value={this.state.StartDate.format()}
+          valueChanged={this.startDateChanged}
         />
+        <p>Pick an End Date</p>
+        <p>EndDate: {this.state.EndDate.format()}</p>
         <Calendar
           className="wijCalendar2"
-          value={this.state.Today}
-          /* Correct*/
-
-          min={this.state.MinEndDate}
-          max={this.state.MaxEndDate}
+          value={this.state.EndDate.format()}
+          min={moment(this.state.StartDate).add("days",1).format()}
+          max={moment(this.state.StartDate).add("days",7).format()}
         />
+        <Calendar
+          className="wijCalendar3"
+          min={moment(this.state.StartDate).add("days",1).format()}
+          max={moment(this.state.StartDate).add("days",7).format()}
+          value={this.state.EndDate.format()}
+        />
+
       </div>
     );
   }
